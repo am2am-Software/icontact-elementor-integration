3523c7770115fb736366e3ca4a559d0ecf2f1001
zakirsujon <zakir021063008@gmail.com>
2024-08-01 19:29:17 +0600

    add: widget output

---
+++ inc/actions/icontact.php
@@ -0,0 +1,174 @@
+<?php
+
+use \Elementor\Controls_Manager;
+
+class WPIEI_iContact extends \ElementorPro\Modules\Forms\Classes\Action_Base
+{
+	public function get_name(){
+		return 'icontact';
+	}
+
+
+	public function get_label(){
+		return esc_html__( 'iContact', 'icontact-elementor-integration' );
+	}
+
+
+	public function register_settings_section( $widget ) {
+		$widget->start_controls_section(
+			'icontact_api_section',
+			[
+				'label' => esc_html__( 'iContact API', 'icontact-elementor-integration' ),
+				'condition' => [
+					'submit_actions' => $this->get_name(),
+				],
+			]
+		);
+
+		$widget->add_control(
+			'icontact_api_appid',
+			[
+				'label' => esc_html__( 'Application ID', 'icontact-elementor-integration' ),
+				'type' => Controls_Manager::TEXT,
+			]
+		);
+
+		$widget->add_control(
+			'icontact_api_username',
+			[
+				'label' => esc_html__( 'Username / Email', 'icontact-elementor-integration' ),
+				'type' => Controls_Manager::TEXT,
+			]
+		);
+
+		$widget->add_control(
+			'icontact_api_password',
+			[
+				'label' => esc_html__( 'Password', 'icontact-elementor-integration' ),
+				'type' => Controls_Manager::TEXT,
+			]
+		);
+
+		$widget->add_control(
+			'icontact_api_url',
+			[
+				'label' => esc_html__( 'API URL', 'icontact-elementor-integration' ),
+				'type' => Controls_Manager::URL,
+			]
+		);
+
+		$widget->add_control(
+			'icontact_api_field_email',
+			[
+				'label' => esc_html__( 'Email Field ID', 'icontact-elementor-integration' ),
+				'type' => Controls_Manager::TEXT,
+			]
+		);
+
+		$widget->add_control(
+			'icontact_api_field_name',
+			[
+				'label' => esc_html__( 'Name Field ID', 'icontact-elementor-integration' ),
+				'type' => Controls_Manager::TEXT,
+			]
+		);
+
+		$widget->add_control(
+			'icontact_api_list_id',
+			[
+				'label' => esc_html__( 'List ID', 'icontact-elementor-integration' ),
+				'type' => Controls_Manager::TEXT,
+			]
+		);
+
+		$widget->end_controls_section();
+	}
+
+
+	public function run( $record, $ajax_handler ) {
+		$settings = $record->get( 'form_settings' );
+
+		if(
+			empty($settings['icontact_api_appid']) ||
+			empty($settings['icontact_api_username']) ||
+			empty($settings['icontact_api_password']) ||
+			empty($settings['icontact_api_url']) ||
+			empty($settings['icontact_api_field_email']) ||
+			empty($settings['icontact_api_list_id'])
+		){
+			return;
+		}
+
+		$raw_fields = $record->get( 'fields' );
+
+		$fields = [];
+		foreach ( $raw_fields as $id => $field ) {
+			$fields[ $id ] = $field['value'];
+		}
+
+		$icontact_headers = [
+			'Accept' => 'application/json',
+			'Content-Type' => 'application/json',
+			'API-Version' => '2.2',
+			'API-AppId' => $settings['icontact_api_appid'],
+			'API-Username' => $settings['icontact_api_username'],
+			'API-Password' => $settings['icontact_api_password']
+		];
+
+		$firstname = empty($settings['icontact_api_field_name']) ? '' : $fields[$settings['icontact_api_field_name']];
+
+		$icontact_data = [
+			[
+				'email' => $fields[$settings['icontact_api_field_email']],
+				'firstName' => $firstname
+			]
+		];
+
+		$response = wp_remote_post(
+			$settings['icontact_api_url']['url'] . 'contacts/',
+			[
+				'headers' => $icontact_headers,
+				'body' => wp_json_encode($icontact_data)
+			]
+		);
+
+		if ( is_wp_error( $response ) ) {
+			$error = $response->get_error_message();
+			// error_log(print_r($error, true));
+		} else {
+			$data = json_decode( wp_remote_retrieve_body( $response ), true );
+			// error_log(print_r($data, true));
+
+			if( isset( $data['contacts'] ) ){
+				foreach( $data['contacts'] as $contact ){
+					if( isset( $contact['contactId'] ) ){
+						$res = wp_remote_post(
+							$settings['icontact_api_url']['url'] . 'subscriptions/',
+							[
+								'headers' => $icontact_headers,
+								'body' => wp_json_encode([
+									[
+										'contactId' => $contact['contactId'],
+										'listId' => $settings['icontact_api_list_id'],
+										'status' => 'normal'
+									]
+								]),
+							]
+						);
+
+						if ( is_wp_error( $res ) ) {
+							$error_msg = $res->get_error_message();
+							// error_log(print_r($error_msg, true));
+						}
+					}
+				}
+			}
+		}
+
+	}
+
+
+	public function on_export( $element ) {
+
+	}
+}
