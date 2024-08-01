fe17e7dbe0a2ff5d3447725ed8ffc3c2a62d1892
zakirsujon <zakir021063008@gmail.com>
2024-08-01 12:40:45 +0600

    add: plugin details

---
+++ icontact-elementor-integration.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Plugin Name: iContact Elementor Integration
+ * Plugin URI: https://am2am.com/plugins/icontact-elementor-integration/
+ * Description: A simple plugin to connect iContact with Elementor form.
+ * Version: 1.0
+ * Requires at least: 5.7
+ * Requires PHP: 7.2
+ * Author: am2am Software
+ * Author URI: https://am2am.com/
+ * License: GPLv2
+ * License URI: https://www.gnu.org/licenses/gpl-2.0.html
+ * Text Domain: icontact-elementor-integration
+ * Domain Path: /languages
+ */
